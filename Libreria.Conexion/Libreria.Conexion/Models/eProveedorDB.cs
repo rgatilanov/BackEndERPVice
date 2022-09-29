9f3ebfa8747471ac4decc18a4e32d9e18f6b8644
Gerardo <Villalobos>
2022-09-29 11:33:08 -0500

    Definición de enums en Models

---
+++ Libreria.Conexion/Libreria.Conexion/Models/eProveedorDB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria.Conexion.Models
+{
+    public enum eProveedorDB
+    {
+        Sql,
+        MySql
+    }
+}
---
+++ Libreria.Conexion/Libreria.Conexion/Models/eTipoDato.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria.Conexion.Models
+{
+    public enum TipoDato
+    {
+        Numerico,
+        Cadena,
+        Bit,
+        Fecha,
+        Hora
+    }
+}

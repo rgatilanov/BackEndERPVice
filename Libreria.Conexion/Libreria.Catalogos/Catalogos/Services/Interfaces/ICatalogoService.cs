ae497f8d261b4ad3e1f0446f40268db4c45674ec
Gerardo <Villalobos>
2022-09-29 13:37:31 -0500

    Creación de Services->Interfaces-> ICatalogoService

---
+++ Libreria.Conexion/Libreria.Catalogos/Catalogos/Services/Interfaces/ICatalogoService.cs
@@ -0,0 +1,29 @@
+﻿using Libreria.ERP.Catalogos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria.ERP.Catalogos.Services.Interfaces
+{
+    public interface ICatalogoService: IDisposable
+    {
+        public List<Ciudad> ConsultarCiudades(int IdEstado);
+
+        #region IDisposable Members
+        public void Dispose()
+        {
+            try
+            {
+                //sqlCon.Dispose();
+                //sqlCon = null;
+                //_Parametros.Clear();
+                //media.Close();
+                //media = null;
+            }
+            catch { }
+        }
+        #endregion
+    }
+}

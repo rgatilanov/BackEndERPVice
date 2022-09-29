9ab767bb031efc676a07806845d8667c3ebd79ca
Gerardo <Villalobos>
2022-09-29 11:35:36 -0500

    Definición de IConexionDB

---
+++ Libreria.Conexion/Libreria.Conexion/Interfaces/IConexionDB.cs
@@ -0,0 +1,20 @@
+﻿using Dapper;
+using Libreria.Conexion.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria.Conexion.Interfaces
+{
+    public interface IConexionDB<T>: IDisposable
+    {
+        void PrepararProcedimiento(string nombreProcedimiento, DynamicParameters dynParameters, CommandType enuTipoComando = CommandType.StoredProcedure);
+        long ExecuteDapper();
+        T QueryFirstOrDefaultDapper();
+        object QueryFirstOrDefaultDapper(TipoDato tipo);
+        IEnumerable<T> Query();
+    }
+}

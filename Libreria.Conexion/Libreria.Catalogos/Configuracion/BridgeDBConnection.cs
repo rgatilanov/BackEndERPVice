6507bb7cacc6779ba509201c5f296311b4764698
Gerardo <Villalobos>
2022-09-29 12:56:17 -0500

    Creación de Librería.ERP y clases de folder Configuracion [primero BridgeDBConnection y después ConnectionStrings]

---
+++ Libreria.Conexion/Libreria.Catalogos/Configuracion/BridgeDBConnection.cs
@@ -0,0 +1,19 @@
+﻿using Libreria.Conexion;
+using Libreria.Conexion.Interfaces;
+using Libreria.Conexion.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria.ERP.Configuracion
+{
+    public class BridgeDBConnection<T>
+    {
+        public static IConexionDB<T> Create(string ConnectionString, eProveedorDB DB)
+        {
+            return Factorizador<T>.Create(ConnectionString, DB);
+        }
+    }
+}
---
+++ Libreria.Conexion/Libreria.Catalogos/Configuracion/ConnectionStrings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria.ERP.Configuracion
+{
+    public static class ConnectionStrings
+    {
+        /// <summary>
+        /// Borrar esto por seguridad: Data Source=localhost;Database=dbVoyagePruebas;user id=erpvice;password=123123
+        /// </summary>
+        public static string LocalServer_SQLServer = "rE0B+cLUyLTNWQfjFaThw+sPlqSjGJQE6w6a+RKoyP255HCBJompUigOyWRS0VznFViJ4sFSTma9ilVnLyWG316ZdjOTuzq8fOV07VRZ3jPO0uDIYPWufe1Z/2x0QQyAYYxDcveZEgd0wWf12SCHESsn+MO7+rRgxmiyyDP3PeW1WpbjSNGUFU4bvscdy82EY5lBuJZRhhbbDoMRbiW0nQ==";
+
+        public static string Azure_SQLServer = String.Empty;
+    }
+}
